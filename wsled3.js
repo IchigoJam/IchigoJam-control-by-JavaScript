6f1d50f979247e57256e3d1d7cdb225f3897b76b
Taisuke Fukuno <fukuno@jig.jp>
2020-03-21 15:15:30 +0900

    update url

--- wsled3.js
+++ wsled3.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 const main = async function() {
   ij.command('CLV:WS.LED20')
 
-  const url = 'http://localhost:8003/api/covid19japan_summary.json'
+  const url = 'https://app.sabae.cc/api/covid19japan_summary.json'
   const data = await (await fetch(url)).json()
   const ratio = 1 - data.ncurrentpatients / data.nbedforinfection
   console.log('現在患者数', data.ncurrentpatients, '感染病床数', data.nbedforinfection, '空き病床数（推測）', ratio)
