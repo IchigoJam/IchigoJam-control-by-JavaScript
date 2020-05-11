66f0369b69f66ae0d0ad8894935b51032403e936
Taisuke Fukuno <fukuno@jig.jp>
2020-05-12 08:17:42 +0900

    shorten led4.mjs

--- led4.mjs
+++ led4.mjs
@@ -1,8 +1,7 @@
 import navigator from './WebGPIO.mjs'
-
 const sleep = async msec => new Promise(resolve => setTimeout(resolve, msec))
 
-const main = async function () {
+const main = async () => {
   const gpio = await navigator.requestGPIOAccess()
   const port = gpio.ports.get('LED')
   await port.export('out') // export (activate) the port.
